ab16a548db3a8202420b1199d8a6a147fbc2f6d1
annek <annek03@univie.ac.at>
2024-03-13 15:02:12 +0100

    7874 Fixed to hide picture of list

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024012302;
+$plugin->version = 2024012303;
 $plugin->release = "v4.3.0";       // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2023100900;      // Requires this Moodle version!
