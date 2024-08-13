63b852d30a471212109bfa0e2f4f7c1fd9262b4d
Martin Reichel <martin.w.reichel@univie.ac.at>
2024-08-13 08:12:19 +0000

    Update 2 files
    - /version.php
    - /CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,5 +3,4 @@ CHANGELOG
 
 4.4.0 (2024-07-17)
 ------------------
-
 * First release for Moodle 4.4
--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024071700;
 $plugin->release = "v4.4.0";       // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2023100900;      // Requires this Moodle version!
+$plugin->requires = 2024042200;      // Requires this Moodle version!
 $plugin->component = 'report_grouptool';    // To check on upgrade, that module sits in correct place.
-$plugin->dependencies = ['mod_grouptool' => 2023070800]; // requires this moodle version
+$plugin->dependencies = ['mod_grouptool' => 2024073000]; // requires this moodle version
