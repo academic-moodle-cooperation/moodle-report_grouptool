c920869e134c9ebd267d1aae0ce6c3b4a2b7a304
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2025-01-28 21:13:21 +0100

    prepare and add lang file de for testing

---
+++ lang/de/report_grouptool.php
@@ -0,0 +1,81 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'report_grouptool', language 'de'
+ *
+ * @package   report_grouptool
+ * @author    Anne Kreppenhofer
+ * @copyright 2023 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org} * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['already_registered'] = '<strong>{$a->username}</strong> ist bereits in Gruppe <strong>{$a->groupname}</strong> registriert!';
+$string['availabledate'] = 'Anmeldebeginn';
+$string['availabledateno'] = 'Immer verfügbar';
+$string['condition_prevent_access'] = 'Die derzeitigen Umstände erlauben Ihnen keinen Zugriff auf die Gruppenverwaltung!';
+$string['download'] = 'Herunterladen';
+$string['download_ods'] = 'ODS';
+$string['download_options'] = 'Herunterladen als';
+$string['download_pdf'] = 'PDF';
+$string['download_txt'] = 'TXT';
+$string['download_xlsx'] = 'XLSX';
+$string['duedate'] = 'Anmeldeende';
+$string['duedateno'] = 'Keine Deadline';
+$string['eventuserlistexported'] = 'Exportierte Teilnehmer/innenliste';
+$string['free'] = 'Frei';
+$string['group_not_in_grouping'] = 'Gewählte Gruppe ist nicht in gewählter Gruppierung!';
+$string['groupoverview'] = 'Gruppenübersicht';
+$string['grouptool'] = 'Gruppenverwaltung';
+$string['grouptool:export'] = 'Exportieren Sie Gruppen- und Registrierungsdaten in verschiedene Formate.';
+$string['grouptool:register'] = 'Registrieren Sie sich selbst in einer aktiven Gruppe mithilfe der Gruppenverwaltung.';
+$string['grouptool:view'] = 'Gruppenverwaltung Bericht anzeigen';
+$string['grouptool:view_description'] = 'Gruppenverwaltung Beschreibung anzeigen';
+$string['grouptool:view_groups'] = 'Aktive Gruppen anzeigen';
+$string['grouptool:view_regs_course_view'] = 'Eine Benutzerliste anzeigen, die zeigt, wer in welcher aktiven Gruppe mit dem Gruppenverwaltung registriert/warteliste ist.';
+$string['grouptool:view_regs_group_view'] = 'Eine gruppierte Liste anzeigen, die zeigt, wer in welcher aktiven Gruppe mit dem Gruppenverwaltung registriert/warteliste ist';
+$string['landscape'] = 'Querformat';
+$string['modulename'] = 'Gruppenverwaltung Bericht';
+$string['no_data_to_display'] = 'Keine Gruppendaten anzuzeigen!';
+$string['no_groups_to_display'] = 'Keine Gruppe(n) anzuzeigen!';
+$string['no_registrations'] = 'Keine Anmeldungen';
+$string['no_users_to_display'] = 'Keine Teilnehmer/innen anzuzeigen!';
+$string['nobody_queued'] = 'Keine Wartelisteneinträge';
+$string['nogrouptools'] = 'Es gibt keine Gruppenverwaltungen!';
+$string['nowhere_queued'] = 'Keine Wartelisteneinträge';
+$string['orientation'] = 'PDF-Ausrichtung';
+$string['overview'] = 'Überblick';
+$string['pluginname'] = 'Gruppenverwaltung Bericht';
+$string['portrait'] = 'Hochformat';
+$string['privacy:metadata'] = 'Das Grouptool Reports-Plugin speichert keine persönlichen Daten.';
+$string['queue'] = 'Warteliste';
+$string['queued'] = 'In Warteliste';
+$string['queues'] = 'Wartelisten';
+$string['rank'] = 'Rang';
+$string['registered'] = 'Angemeldet';
+$string['registrations'] = 'Anmeldungen';
+$string['status'] = 'Status';
+$string['status_help'] = '<ul>
+<li><strong>✔</strong> angemeldet in Moodle-Gruppe und Gruppenverwaltung</li>
+<li><strong>?</strong> angemeldet in Moodle-Gruppe, nicht aber in der Gruppenverwaltung</li>
+<li><strong>+</strong> angemeldet in der Gruppenverwaltung, nicht aber in Moodle-Gruppe</li>
+<li><strong>1, 2, 3...</strong> auf Warteliste in der Gruppenverwaltung</li>
+</ul>';
+$string['switched_to_all_groups'] = 'Ändere Gruppenfilter zu "Alle"!';
+$string['total'] = 'Gesamt';
+$string['userlist'] = 'Teilnehmer/innenliste';
