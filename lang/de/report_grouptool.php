4c0a7d2539092745e9176b26dde62a363b0f060e
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2025-01-28 21:26:04 +0100

    codechecker issue

--- lang/de/report_grouptool.php
+++ lang/de/report_grouptool.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -21,6 +20,7 @@
  * @package   report_grouptool
  * @author    Anne Kreppenhofer
  * @copyright 2023 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org} * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
